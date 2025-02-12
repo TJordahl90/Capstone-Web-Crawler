1a8b9813adeaa0f6e8d246106f2baae167a1f7c3
Anh Vo <anhvo2@my.unt.edu>
2025-02-11 18:03:01 -0600

    Updated frontend with new Landing Page design

---
+++ frontend/src/LandingPage.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./LandingPage.css";
+import knobLogo from "./assets/knob.png";
+
+const LandingPage = () => {
+  return (
+    <div className="landing-container">
+      {/* Navigation Bar */}
+      <nav className="navbar">
+        <button className="btn signup">Sign up</button>
+        <button className="btn login">Log in</button>
+      </nav>
+
+      {/* Logo */}
+      <div className="logo-container">
+        <img src={knobLogo} alt="Knob Logo" className="logo" />
+      </div>
+
+      {/* Hero Section (Positioned Text) */}
+      <h2 className="opportunity-text">
+        Opportunities <br />
+        <br />
+        <br />
+        to Connections
+      </h2>
+
+      <h2 className="connection-text">to Connections</h2>
+
+      {/* Right-Aligned Ellipses */}
+      <div className="large_white_ellipse"></div>
+      <div className="large_black_ellipse"></div>
+
+      {/* Other Background Shapes */}
+      <div className="small_white_rectangle"></div>
+      <div className="small_white_ellipse"></div>
+    </div>
+  );
+};
+
+export default LandingPage;
