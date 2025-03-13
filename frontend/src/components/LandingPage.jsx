dafe3a1f2657529919b24af6d705bda1b2ef00bb
Anh Vo <anhvo8438@gmail.com>
2025-03-13 16:26:57 -0500

    Refactored styles, cleaned up UI structure

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,36 +1,22 @@
 import React from "react";
 import "./LandingPage.css";
-import knobLogo from "../assets/knob.png";
 
 const LandingPage = () => {
   return (
     <div className="landing-container">
-      {/* Navigation Bar */}
-      {/* <nav className="navbar">
-        <button className="btn signup">Sign up</button>
-        <button className="btn login">Log in</button>
-      </nav> */}
+      {/* Navbar (Now inside Landing Page) */}
+      <nav className="navbar">
+        <a href="/register" className="btn signup">Register</a>
+        <a href="/login" className="btn login">Login</a>
+      </nav>
 
-      {/* Logo */}
-      {/* <div className="logo-container">
-        <img src={knobLogo} alt="Knob Logo" className="logo" />
-      </div> */}
-
-      {/* Hero Section (Positioned Text) */}
-      <h2 className="opportunity-text">
-        Opportunities <br />
-        <br />
-        <br />
-        to Connections
-      </h2>
-
+      {/* Hero Section */}
+      <h2 className="opportunity-text">Opportunities</h2>
       <h2 className="connection-text">to Connections</h2>
 
-      {/* Right-Aligned Ellipses */}
+      {/* Background Shapes */}
       <div className="large_white_ellipse"></div>
       <div className="large_black_ellipse"></div>
-
-      {/* Other Background Shapes */}
       <div className="small_white_rectangle"></div>
       <div className="small_white_ellipse"></div>
     </div>
