18fe6fb367406a11dc830eeb0b0c63bc761e883e
EarlAmematekpo16 <165102260+kwa0028@users.noreply.github.com>
2025-03-20 19:51:49 -0500

    Update urls.py
    should be able to register new user

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import CreateUserView, UserProfileView
 
 urlpatterns = [
-    path('register/', views.CreateUserView.as_view(), name="create_user"),
+    path('register/', CreateUserView.as_view(), name="create_user"),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', UserProfileView.as_view(), name='user_profile'),
 ]
+
+#to try to register a new user http://127.0.0.1:8000/api/register/
