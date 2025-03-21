ab65931714e6b563cab2b9fa7a7baab5bb63b25f
EarlAmematekpo16 <165102260+kwa0028@users.noreply.github.com>
2025-03-20 19:53:08 -0500

    Update views.py
    Added login view and User profile view

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,8 +3,11 @@ from .serializers import *
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
 # Create your views here.
 
 class CreateUserView(APIView):
@@ -18,3 +21,28 @@ class CreateUserView(APIView):
             serializer.save() # saves serialized valid data
             return Response(serializer.data, status=status.HTTP_201_CREATED) # sends response to frontend
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) # or sends error msg
+
+class LoginView(APIView):
+    """Handles user authentication and returns JWT tokens"""
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            })
+        return Response({"error": "Invalid credentials"}, status=400)
+
+class UserProfileView(APIView):
+    """Fetches authenticated user details"""
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = CreateUserSerializer(request.user)
+        return Response(serializer.data)
