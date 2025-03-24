3b4ba83d79057094ae7e16dff88b4fd316cfb247
TJordahl90 <jordahl902002@gmail.com>
2025-03-24 16:14:21 -0500

    Reason we couildn't log onto the admin page was SESSION_COOKIE_SAMESITE. Commented it out for now

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -139,7 +139,7 @@ CSRF_TRUSTED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_COOKIE_SAMESITE = 'None'
-SESSION_COOKIE_SAMESITE = 'None'
+#SESSION_COOKIE_SAMESITE = 'None' # This line is the reason we were being redirected instead of logged on to the admin page. Not sure how to fix it at the moment
 
 CSRF_COOKIE_HTTPONLY = False
 SESSION_COOKIE_HTTPONLY = True
