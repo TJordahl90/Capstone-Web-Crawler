d70df6454dce848c95b0f09639933528cd6fcdf7
TJordahl90 <jordahl902002@gmail.com>
2025-03-27 16:35:05 -0500

    Commented a testing line

--- frontend/src/components/AuthForm.jsx
+++ frontend/src/components/AuthForm.jsx
@@ -13,14 +13,14 @@ const AuthForm = ({ isLogin }) => {
         last_name: '',
         email: '',
     });
+
     const [message, setMessage] = useState('');
     const [error, setError] = useState('');
     
     const handleSubmit = async (e) => {
         e.preventDefault();
         const endpoint = isLogin ? "/login/" : "/register/";
-
-        const temp = await api.post("/verification/", formData)
+        const temp = await api.post("/verification/", formData); // Feel free to get rid of this or move it this was just to test the verification table in the database
 
         try {
             const response = await api.post(endpoint, formData);
