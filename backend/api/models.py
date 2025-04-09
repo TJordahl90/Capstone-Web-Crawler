7727e8edc303f4743f43d1d3c297d8dce29cd2f1
TJordahl90 <jordahl902002@gmail.com>
2025-04-09 16:25:51 -0500

    Created job requirements linking table

--- backend/api/models.py
+++ backend/api/models.py
@@ -22,7 +22,7 @@ class JobPosting(models.Model):
     company = models.CharField(max_length=150)
     title = models.CharField(max_length=100)
     description = models.TextField()  # TextField is better for long text
-    requirements = ArrayField(models.CharField(max_length=20), size=15, default=list)
+    requirements = models.ManyToManyField(CommonSkills, related_name='job_posting', blank=True)
     location = models.CharField(max_length=50, blank=True, null=True)  # Optional is good
     datePosted = models.DateField(null=True, blank=True)  # Allow NULL and blank values
     salary = models.CharField(max_length=255, blank=True, null=True)  # Optional seems better
