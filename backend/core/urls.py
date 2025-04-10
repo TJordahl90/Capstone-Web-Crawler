346f22e4bca874a903254ec55bf59685455bbc3a
jake2255 <157094404+jake2255@users.noreply.github.com>
2025-04-09 21:33:18 -0500

    download and setup pillow library for images

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,22 +1,8 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 def home_view(request):
     return JsonResponse({"message": "Welcome to the Capstone API"}, status=200)
@@ -27,4 +13,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-#to try to register a new user http://127.0.0.1:8000/api/register/
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
