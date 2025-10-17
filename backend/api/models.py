e48b11ec416c87f7f727e1b633de70e993b35471
TJordahl90 <jordahl902002@gmail.com>
2025-10-17 15:23:37 -0500

    Modified db, ran the summarizer/requirement gatherer

--- backend/api/models.py
+++ backend/api/models.py
@@ -68,6 +68,8 @@ class JobPosting(models.Model):
     datePosted = models.CharField(max_length=50, null=True, blank=True)
     salary = models.CharField(max_length=255, blank=True, null=True)  # Optional seems better
     jobURL = models.URLField(max_length=500)
+    summary = models.TextField(blank=True, null=True)
+    minimumRequirements = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f'{self.title} at {self.company}'
