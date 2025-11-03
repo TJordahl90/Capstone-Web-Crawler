50107c69fbf2bfa64bdc0c6d0b2f375ede618399
jake2255 <157094404+jake2255@users.noreply.github.com>
2025-11-03 09:27:53 -0600

    lockheed done (as good as this gets)

--- backend/api/management/commands/scrape_jobs.py
+++ backend/api/management/commands/scrape_jobs.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from api.models import *
-from api.web_scrapers import TexasInstruments, LockheedMartin, JSearchAPI
+from api.web_scrapers import TexasInstruments, LockheedMartinSoftware, LockheedMartinSystems, LockheedMartinOtherTech, JSearchAPI
 
 class Command(BaseCommand):
     """Runs all web scrapers in the 'web_scraper' folder"""
@@ -10,8 +10,10 @@ class Command(BaseCommand):
         # list all scrapers here
         scrapers = [
             # Fugetec.fugetec,
-            TexasInstruments.TexInstr,
-            # LockheedMartin.lockheed_scraper
+            # TexasInstruments.TexInstr,
+            LockheedMartinSoftware.lockheed_scraper,
+            # LockheedMartinSystems.lockheed_scraper,
+            # LockheedMartinOtherTech.lockheed_scraper,
             # JSearchAPI.jsearch_api
         ]
 
