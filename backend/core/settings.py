c0148290c686f88bf68a7991540876c35bd18c97
TJordahl90 <jordahl902002@gmail.com>
2025-11-05 17:27:57 -0600

    media root

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # will need media stuff, forgot about resume saving
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/app/media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
