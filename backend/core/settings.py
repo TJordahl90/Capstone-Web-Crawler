7658c7fcfae6fad5536df9e2bb88a6a912508296
TJordahl90 <jordahl902002@gmail.com>
2025-11-08 00:05:52 -0600

    Migrated db to mirror prod server db and updated base_dir

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -142,7 +142,7 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
 STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
